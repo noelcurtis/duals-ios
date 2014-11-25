99da9df7f96360de11da010833904217ba9a9b50
Noel Curtis <ncurtis@grubhub.com>
2014-11-25 08:40:42 -0500

    Updated to connect cells.

---
+++ iphone/Duals/Duals/Src/Views/CreateLadderCell.swift
@@ -0,0 +1,24 @@
+//
+//  CreateLadderCell.swift
+//  Duals
+//
+//  Created by ncurtis on 11/25/14.
+//  Copyright (c) 2014 noelcurtis. All rights reserved.
+//
+
+import UIKit
+
+class CreateLadderNameCell: UITableViewCell {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
+
+
+class CreateLadderActivityCell: UITableViewCell {
+    
+    @IBOutlet weak var activityTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
